b586a7fff2877b718ff5c3af68fc6caed094e390
Alissa Pajer <alissa.pajer@gmail.com>
2018-04-05 15:47:10 -0600

    printlns

--- connector/src/main/scala/quasar/fs/Local.scala
+++ connector/src/main/scala/quasar/fs/Local.scala
@@ -429,6 +429,7 @@ class Local private (baseDir: JFile) {
   private def delete(path: APath): Task[LocalFSError[FSError[Unit]]] =
     Task delay {
       val jpath: JPath = toJPath(path)
+      println(s"delete called with $jpath")
 
       failIfSourceMissing(path) match {
         case Some(err) => err.left.right
@@ -436,11 +437,13 @@ class Local private (baseDir: JFile) {
           case -\/(_) => // directory
             val visitor = new SimpleFileVisitor[JPath] {
               override def visitFile(file: JPath, attrs: BasicFileAttributes): FileVisitResult = {
+                println(s"visiting file: $file")
                 Files.delete(file)
                 FileVisitResult.CONTINUE
               }
 
               override def postVisitDirectory(dir: JPath, exc: IOException): FileVisitResult = {
+                println(s"visiting post dir: $dir")
                 Files.delete(dir)
                 FileVisitResult.CONTINUE
               }
